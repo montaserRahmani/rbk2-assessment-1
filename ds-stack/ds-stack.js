86a84f3c5e47cabef997e432183af7fa03a22232
montaserRahmani <montaser.rah@gmail.com>
2017-01-07 09:39:51 +0200

    stack solved

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,17 @@
 var Stack = function() {
-	//your code is here
+	var obj = {};
+	obj._storage=[];
+	return _.extend(obj, stackMethods);
 };
+
+var stackMethods = {};
+
+// i was wondering is it this simple you want ?! because you requested just these three properties.
+
+stackMethods.add = function(value){
+	this._storage.push(value);
+}
+
+stackMethods.remove = function(){
+	return this._storage.pop();
+}
