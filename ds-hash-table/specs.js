d3621e59328c3df299c3fbb53ef3a060a93e54d1
montaserRahmani <montaser.rah@gmail.com>
2017-01-07 10:03:33 +0200

    add some tests for hashTable and Stack

--- ds-hash-table/specs.js
+++ ds-hash-table/specs.js
@@ -1,5 +1,5 @@
 var assert = chai.assert;
-
+var expect = chai.expect;
 describe('ds-hash-table', function() {
 	
   it('should insert values correctly', function() {
@@ -8,6 +8,22 @@ describe('ds-hash-table', function() {
    	hashTable.insert("k2", "v2");
    	var result = hashTable.retrieve("k1");
    	expect(result).to.eql("v1");
+  });
+
+  it('should not add new key if its already exists', function() {
+  	var hashTable = makeHashTable();	
+   	hashTable.insert("k1", "v1");
+   	hashTable.insert("k1", "v2");
+   	var result = hashTable.retrieve("k1");
+   	expect(result).to.eql("v2");
+  });
+
+  it('should not have collisions of same index', function() {
+  	var hashTable = makeHashTable();	
+   	hashTable.insert("k", "v1"); // generate index 3 same as "kk"
+   	hashTable.insert("kk", "v2");
+   	var result = hashTable.retrieve("kk");
+   	expect(result).to.eql("v2");
   });
 
 });
--- ds-stack/specs.js
+++ ds-stack/specs.js
@@ -1,9 +1,21 @@
 var assert = chai.assert;
-
+var expect = chai.expect;
 describe('ds-stack', function() {
 
-  it('should test something', function() {
-    assert.equal(2+2, 4);
+  it('should insert values correctly', function() {
+  	var stack = Stack();	
+   	stack.add(1);
+   	stack.add(2);
+   	var result = stack._storage[1];
+   	expect(result).to.eql(2);
+  });
+
+  it('should remove the last value and return it', function() {
+  	var stack = Stack();	
+   	stack.add(1);
+   	stack.add(3);
+   	var result = stack.remove();
+   	expect(result).to.eql(3);
   });
 
 });
